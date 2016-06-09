8e41cc82a2cc0c6c33da5e69e29568b23dbe27c3
Marcos Minond <minond.marcos@gmail.com>
2016-06-08 20:58:25 -0500

    leaf knowledge

--- src/knowledge.js
+++ src/knowledge.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const Life = require('./life');
 const d = require('./utils').d;
 const rand = require('./utils').rand;
+const rand_i = require('./utils').rand_i;
+const has = require('./utils').has;
+const get = require('./utils').get;
 const set = require('./utils').set;
 const unset = require('./utils').unset;
 const likelihood = require('./utils').likelihood;
@@ -19,7 +23,7 @@ function death_knowledge(obj) {
     }
 }
 
-function asexual_reproduction_knowledge(obj) {
+function random_asexual_reproduction_knowledge(obj) {
     if (likelihood(4)) {
         d('%s is reproducing', obj.toString());
         obj.world.add(obj.clone());
@@ -34,14 +38,30 @@ function random_death_knowledge(obj) {
     }
 }
 
+function sibling_placement_knowledge(obj) {
+    var coor, coors;
+
+    if (!obj.memory.sibling_placement_knowledge) {
+        unset(obj.world, [obj.x, obj.y], obj);
+        coors = around_coors(obj);
+
+        for (var i = 0, len = coors.length; i < len; i++) {
+            if (!has(get(obj.world, [coors[i].x, coors[i].y]), Life, true, obj)) {
+                coor = coors[i];
+                obj.x = coor.x;
+                obj.y = coor.y;
+                obj.memory.sibling_placement_knowledge = true;
+                set(obj.world, [obj.x, obj.y], obj);
+                return;
+            }
+        }
+
+    }
+}
+
 function move_knowledge(obj) {
     var last = obj.memory.move_knowledge || { x: obj.x, y: obj.y },
         next;
-
-    if (obj.freeze) {
-        d('%s staying in [%s, %s]', obj.toString(), last.x, last.y);
-        return;
-    }
 
     next = rand(around_coors(obj), last);
 
@@ -54,15 +74,16 @@ function move_knowledge(obj) {
 }
 
 module.exports = {
-    asexual_reproduction_knowledge,
-    random_death_knowledge,
-    move_knowledge,
-
     ANT_KNOWLEDGE: [
-        asexual_reproduction_knowledge,
+        random_asexual_reproduction_knowledge,
         random_death_knowledge,
         move_knowledge,
         death_knowledge,
         hunger_knowledge,
-    ]
+    ],
+
+    LEAF_KNOWLEDGE: [
+        sibling_placement_knowledge,
+        random_asexual_reproduction_knowledge,
+    ],
 };
